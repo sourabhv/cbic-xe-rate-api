7f9720d156255a537253ebf219c52d06775c1f86
Sourabh Verma <sourabh.coder@gmail.com>
2022-01-16 03:24:38 +0530

    Clean up file structure

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { fetchURLs } from "./cbic-urls";
-import { Currency, parseRate } from "./pdf-parserr";
+import { Currency, parseRate } from "./pdf-parser";
 
 const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -10,8 +10,17 @@ async function fetchRatesForYearAndCurrency(year: number, currency: Currency) {
   for (let i = 0; i < urls.length; i++) {
     console.log(`Parsing ${i}, ${urls[i].url}`);
     const pdfUrl = urls[i].url;
-    const results = await parseRate(pdfUrl, [currency]);
+    let results;
+    try {
+      results = await parseRate(pdfUrl, [currency]);
+    } catch (err) {
+      console.error(`Error fetching ${pdfUrl}`);
+      await delay(5000);
+      results = await parseRate(pdfUrl, [currency]);
+    }
+
     await delay(200);
+
     const currencyResults = results[currency];
     if (currencyResults) {
       rates.push({ ...currencyResults, ...urls[i] });
